71d48a3aa1b44bc9e1069df2231705d7fdff1890
gsiudak <01169236@pw.edu.pl>
2023-04-24 19:31:55 +0200

    a

--- python/server2/z22_server.py
+++ python/server2/z22_server.py
@@ -14,25 +14,6 @@ if __name__ == "__main__":
         s.bind((name, port))
         s.listen(5)
 
-        conn, addr = s.accept()
-
-        full_mess = ""
-        while True:
-            data = conn.recv(63)
-            if not data:
-                break
-            else:
-                print(f"Mess: {data.decode('utf-8')} from {addr}")
-                full_mess += data.decode('utf-8')
-
-        print(f"Full mess: {full_mess}")
-
-        s.close()
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((name, port))
-        s.listen(5)
-
         while True:
             conn, addr = s.accept()
             with conn:
