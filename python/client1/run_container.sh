b3504b587947469f1ab58ab02642bb4cf765c393
gsiudak <01169236@pw.edu.pl>
2023-05-08 20:38:25 +0200

    change in run_container

--- python/client1/run_container.sh
+++ python/client1/run_container.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run --rm -it --network-alias z33_python_client --hostname z33_python_client --network z33_network --name z33_python_client z33_task2_1_python_client $1 $2
+docker run --rm --network-alias z33_python_client1 --hostname z33_python_client --network z33_network --name z33_python_client z33_task2_1_python_client $1 $2
+docker run --rm --network-alias z33_python_client2 --hostname z33_python_client --network z33_network --name z33_python_client z33_task2_1_python_client $1 $2
+docker run --rm --network-alias z33_python_client3 --hostname z33_python_client --network z33_network --name z33_python_client z33_task2_1_python_client $1 $2
+docker run --rm --network-alias z33_python_client4 --hostname z33_python_client --network z33_network --name z33_python_client z33_task2_1_python_client $1 $2
+docker run --rm --network-alias z33_python_client5 --hostname z33_python_client --network z33_network --name z33_python_client z33_task2_1_python_client $1 $2
