acc26927e7193a91ee9453df65fafdc8102501d1
gsiudak <01169236@pw.edu.pl>
2023-05-08 20:51:36 +0200

    a

--- python/client1/run_container.sh
+++ python/client1/run_container.sh
@@ -5,7 +5,7 @@ docker run --rm --network-alias z33_python_client2 --hostname z33_python_client2
 docker run --rm --network-alias z33_python_client3 --hostname z33_python_client3 --network z33_network --name z33_python_client3 z33_task2_1_python_client $1 $2 &
 docker run --rm --network-alias z33_python_client4 --hostname z33_python_client4 --network z33_network --name z33_python_client4 z33_task2_1_python_client $1 $2 &
 docker run --rm --network-alias z33_python_client5 --hostname z33_python_client5 --network z33_network --name z33_python_client5 z33_task2_1_python_client $1 $2;
-
+sleep 1s
 docker run --rm --network-alias z33_python_client1 --hostname z33_python_client1 --network z33_network --name z33_python_client1 z33_task2_1_python_client $1 $2 &
 docker run --rm --network-alias z33_python_client2 --hostname z33_python_client2 --network z33_network --name z33_python_client2 z33_task2_1_python_client $1 $2 &
 docker run --rm --network-alias z33_python_client3 --hostname z33_python_client3 --network z33_network --name z33_python_client3 z33_task2_1_python_client $1 $2 &
