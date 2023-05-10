55a58dd0765299b17092f34dc4d439f1f22681e1
gsiudak <01169236@pw.edu.pl>
2023-05-10 22:25:17 +0200

    teraz ostateczna

--- c/server1/z21_server.c
+++ c/server1/z21_server.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/wait.h>
 
 int main(int argc, char **argv)
 {
