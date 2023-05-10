8e3c9e8a56fca815a4b5d8333aaabe4e907ee79e
gsiudak <01169236@pw.edu.pl>
2023-05-10 22:16:02 +0200

    usuwam wait()

--- c/server1/z21_server.c
+++ c/server1/z21_server.c
@@ -98,7 +98,7 @@ int main(int argc, char **argv)
       {
         printf("Stworzono proces potomny o PID: %d\n", pid);
         close(polaczenie);
-        wait(NULL);
+        // wait(NULL);
       }
     }
   }
